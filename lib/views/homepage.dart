54b97d39513fe1c1e154f871f4b893b43d9132a3
dom <dom@boo.chat>
2018-12-10 17:44:57 +0800

    add flare animation

--- lib/views/homepage.dart
+++ lib/views/homepage.dart
@@ -1,11 +1,12 @@
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 const String _name = "Dom Liang";
 
 
-class HomePage extends StatefulWidget {                     //modified
-  @override                                                        //new
+class HomePage extends StatefulWidget {                     
+  @override                                                       
   State createState() => new HomePageState();                    //new
 } 
 
@@ -27,9 +28,23 @@ class HomePageState extends State<HomePage> with TickerProviderStateMixin {
         new CupertinoButton(
           child: Text('go to chat'),
           onPressed: this._handleSubmitted,
-        )                                                      
+        ),
+        Expanded(
+          child: FlareActor(
+            "assets/Sushi.flr",
+            animation: "Sushi Bounce",
+          ),
+        ),
+        // new FlareActor(
+        //     "assets/Sushi.flr2d",
+        //     animation: "Sushi Bounce",
+        //     alignment: Alignment.center,
+        //     fit: BoxFit.contain,
+        //     // animation: "idle",
+        //   ),
       ],                                                         
     ),
     );
   }
 }
+
